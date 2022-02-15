dbdbfcf93da5f12a796942c41bb8ab53bcd9d442
Joshua-123-ansah <owusuja500@gmail.com>
2022-02-15 22:08:45 +0000

    Containers

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,83 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                color: Colors.white,
-                width: 100.0,
-                height: double.infinity,
-                // child: Text("Container One"),
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("images/josh.jpeg"),
+            ),
+            Text(
+              "Joshua Owusu Ansah",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Pacifico",
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                fontFamily: 'SourceSansPro-Light',
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
               ),
-              Container(
-                color: Colors.red,
-                width: 100.0,
-                height: 200.0,
-                // child: Text("Container Two"),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(9.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "+233 (0) 240235033",
+                    style: TextStyle(
+                      color: Colors.teal.shade400,
+                      fontFamily: "SourceSansPro-Light",
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
               ),
-              Container(
-                color: Colors.blue,
-                width: 100.0,
-                height: double.infinity,
-                // child: Text("Container Three"),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
               ),
-            ],
-          ),
-        ),
+              padding: EdgeInsets.all(9.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "joshua.ansah@ashesi.edu.gh",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: "SourceSansPro-Light",
+                      fontSize: 15.0,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
